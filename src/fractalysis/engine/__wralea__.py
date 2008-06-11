2d57029fa3d87aed2648e6a61dfa5a295c73527c
David Da Silva <dodasilva@ucdavis.edu>
2008-06-11 10:10:52 +0000

    replacing old wralea file
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/fractalysis@5035 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ src/fractalysis/engine/__wralea__.py
@@ -0,0 +1,109 @@
+# -*- python -*-
+#
+#       OpenAlea.Fractalysis : OpenAlea fractal analysis library module
+#
+#       Copyright or (C) or Copr. 2006 INRIA - CIRAD - INRA  
+#
+#       File author(s): Da SILVA David <david.da_silva@cirad.fr>
+#                       Boudon Frederic <frederic.boudon@cirad.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+# 
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+
+from openalea.core import *
+from openalea.core.traitsui import View, Group, Item
+
+__name__ = "fractalysis.engine"
+__version__ = '0.0.1'
+__license__ = 'CeCILL-C'
+__authors__ = 'OpenAlea consortium'
+__institutes__ = 'INRIA/CIRAD'
+__description__ = 'Fractalysis engine nodes.'
+__url__ = 'http://openalea.gforge.inria.fr'
+ 
+__editable__ = 'False' 
+ 
+__all__ = ['BCM', 'Voxelize', 'TwoSurfaces', 'Scene2MatrixLac', 'Lacunarity', ]
+
+###### begin nodes definitions #############
+
+BCM = Factory( name="BCM",
+              description="Apply box counting method on scene",
+              category="Fractal Analysis",
+              nodemodule="engine_nodes",
+              nodeclass="BCM",
+              inputs=(dict(name="Scene", interface=None,),
+                      dict(name="Stop Factor", interface=IInt(min=3), value=10),
+                      ),
+              outputs=(dict(name="Scales", interface = ISequence),
+                       dict(name="Intercepted Voxels", interface = ISequence),),
+              )
+
+Voxelize = Factory( name="Voxelize",
+              description="Generates an embedding grid for a scene",
+              category="Fractal Analysis",
+              nodemodule="engine_nodes",
+              nodeclass="voxelize",
+              inputs=(dict(name="Scene", interface=None,),
+                      dict(name="Division Factor", interface=IInt, value=10),
+                      dict(name="Density", interface=IBool, value=True),
+                      ),
+              outputs=(dict(name="Voxels size", interface = IInt),
+                       dict(name="Centers", interface = ISequence),
+                       dict(name="Densities", interface = ISequence),
+                       dict(name="VoxScene", interface = None),
+                      ),
+              )
+
+
+TwoSurfaces = Factory( name="TwoSurfaces",
+              description="Computes two surfaces on a multiscale scenes",
+              category="Fractal Analysis",
+              nodemodule="twosurfaces",
+              nodeclass="TwoSurfaces",
+              inputs=(dict(name="Leaves", interface=None,),
+                      dict(name="Macrorep", interface=ISequence),
+                      ),
+              outputs=(dict(name="Macrosurfaces", interface = ISequence),
+                       dict(name="Microsurfaces", interface = ISequence),),
+              )
+
+Scene2MatrixLac = Factory( name="Scene2MatrixLac",
+              description="Generate a MatrixLac from PlantGL scene",
+              category="Fractal Analysis",
+              nodemodule="engine_nodes",
+              nodeclass="lactrix_fromScene",
+              inputs=(dict(name="Scene", interface=None,),
+                      dict(name="Name", interface=IStr),
+                      dict(name="Grid Size", interface=IInt(min=2)),
+                      dict(name="Save Directory", interface=IDirStr, value='/tmp'),
+                      dict(name="Density", interface=IBool, value=False),
+                      ),
+              outputs=(dict(name="MatrixLac", interface = None,),
+                       dict(name="Pgl scene", interface = None,),
+                      ),
+              )
+
+
+Lacunarity = Factory( name="Lacunarity",
+              description="Compute lacunarity of n-dimensional matrix",
+              category="Fractal Analysis",
+              nodemodule="engine_nodes",
+              nodeclass="lacunarity",
+              view=View(
+                      Item("Type"),
+                      Group(
+                          "GlidingBox radius",
+                          Item("Start"),
+                          Item("Stop"),
+                          layout='-',
+                          )
+                        ),
+            )
+
+###### end nodes definitions ###############
+
