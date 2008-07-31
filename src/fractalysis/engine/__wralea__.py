928c9cc7112038db6693698ed572c6c831f45ab5
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2008-07-31 15:14:39 +0000

    move fractalysis node
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/fractalysis@5372 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/fractalysis/engine/__wralea__.py
+++ src/fractalysis/engine/__wralea__.py
@@ -1,124 +1,107 @@
-# -*- python -*-
-#
-#       OpenAlea.Fractalysis : OpenAlea fractal analysis library module
-#
-#       Copyright or (C) or Copr. 2006 INRIA - CIRAD - INRA  
-#
-#       File author(s): Da SILVA David <david.da_silva@cirad.fr>
-#                       Boudon Frederic <frederic.boudon@cirad.fr>
-#
-#       Distributed under the Cecill-C License.
-#       See accompanying file LICENSE.txt or copy at
-#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
-# 
-#       OpenAlea WebSite : http://openalea.gforge.inria.fr
-#
+
+# This file has been generated at Thu Jul 31 17:10:33 2008
 
 from openalea.core import *
-from openalea.core.traitsui import View, Group, Item
 
-__name__ = "fractalysis.engine"
-__version__ = '0.0.1'
+
+__name__ = 'vplants.fractalysis.engine'
+
+__editable__ = True
+__description__ = 'Fractalysis engine nodes.'
 __license__ = 'CeCILL-C'
+__url__ = 'http://openalea.gforge.inria.fr'
+__alias__ = ['fractalysis.engine']
+__version__ = '0.0.1'
 __authors__ = 'OpenAlea consortium'
 __institutes__ = 'INRIA/CIRAD'
-__description__ = 'Fractalysis engine nodes.'
-__url__ = 'http://openalea.gforge.inria.fr'
-__icon__= 'engine_icon.png' 
-__editable__ = 'False' 
+__icon__ = 'engine_icon.png'
  
-__all__ = ['BCM', 'Voxelize', 'TwoSurfaces', 'Scene2MatrixLac', 'Pix2MatrixLac', 'Lacunarity', ]
 
-###### begin nodes definitions #############
-
-BCM = Factory( name="BCM",
-              description="Apply box counting method on scene",
-              category="Fractal Analysis",
-              nodemodule="engine_nodes",
-              nodeclass="BCM",
-              inputs=(dict(name="Scene", interface=None,),
-                      dict(name="Stop Factor", interface=IInt(min=3), value=10),
-                      ),
-              outputs=(dict(name="Scales", interface = ISequence),
-                       dict(name="Intercepted Voxels", interface = ISequence),),
-              )
-
-Voxelize = Factory( name="Voxelize",
-              description="Generates an embedding grid for a scene",
-              category="Fractal Analysis",
-              nodemodule="engine_nodes",
-              nodeclass="voxelize",
-              inputs=(dict(name="Scene", interface=None,),
-                      dict(name="Division Factor", interface=IInt, value=10),
-                      dict(name="Density", interface=IBool, value=True),
-                      ),
-              outputs=(dict(name="Voxels size", interface = IInt),
-                       dict(name="Centers", interface = ISequence),
-                       dict(name="Densities", interface = ISequence),
-                       dict(name="VoxScene", interface = None),
-                      ),
-              )
+__all__ = ['twosurfaces_TwoSurfaces', 'engine_nodes_BCM', 'engine_nodes_lactrix_fromPix', 'engine_nodes_lacunarity', 'engine_nodes_lactrix_fromScene', 'engine_nodes_voxelize']
 
 
-TwoSurfaces = Factory( name="TwoSurfaces",
-              description="Computes two surfaces on a multiscale scenes",
-              category="Fractal Analysis",
-              nodemodule="twosurfaces",
-              nodeclass="TwoSurfaces",
-              inputs=(dict(name="Leaves", interface=None,),
-                      dict(name="Macrorep", interface=ISequence),
-                      ),
-              outputs=(dict(name="Macrosurfaces", interface = ISequence),
-                       dict(name="Microsurfaces", interface = ISequence),),
-              )
 
-Scene2MatrixLac = Factory( name="Scene2MatrixLac",
-              description="Generate a MatrixLac from PlantGL scene",
-              category="Fractal Analysis",
-              nodemodule="engine_nodes",
-              nodeclass="lactrix_fromScene",
-              inputs=(dict(name="Scene", interface=None,),
-                      dict(name="Name", interface=IStr),
-                      dict(name="Grid Size", interface=IInt(min=2)),
-                      dict(name="Save Directory", interface=IDirStr, value='/tmp'),
-                      dict(name="Density", interface=IBool, value=False),
-                      ),
-              outputs=(dict(name="MatrixLac", interface = None,),
-                       dict(name="Pgl scene", interface = None,),
-                      ),
-              )
+twosurfaces_TwoSurfaces = Factory(name='TwoSurfaces', 
+                description='Computes two surfaces on a multiscale scenes', 
+                category='Fractal Analysis', 
+                nodemodule='twosurfaces',
+                nodeclass='TwoSurfaces',
+                inputs=({'interface': None, 'name': 'Leaves'}, {'interface': ISequence, 'name': 'Macrorep'}),
+                outputs=({'interface': ISequence, 'name': 'Macrosurfaces'}, {'interface': ISequence, 'name': 'Microsurfaces'}),
+                widgetmodule=None,
+                widgetclass=None,
+                )
 
 
-Pix2MatrixLac = Factory( name="Pix2MatrixLac",
-              description="Generate a MatrixLac from an Image",
-              category="Fractal Analysis",
-              nodemodule="engine_nodes",
-              nodeclass="lactrix_fromPix",
-              inputs=(dict(name="Image path", interface=IFileStr,),
-                      dict(name="Pixel width", interface=IFloat),
-                      dict(name="Save Directory", interface=IDirStr, value='/tmp'),
-                      ),
-              outputs=(dict(name="MatrixLac", interface = None,),
-                       dict(name="Thresholded image", interface = None,),
-                      ),
-              )
 
 
-Lacunarity = Factory( name="Lacunarity",
-              description="Compute lacunarity of n-dimensional matrix",
-              category="Fractal Analysis",
-              nodemodule="engine_nodes",
-              nodeclass="lacunarity",
-              view=View(
-                      Item("Type"),
-                      Group(
-                          "GlidingBox radius",
-                          Item("Start"),
-                          Item("Stop"),
-                          layout='-',
-                          )
-                        ),
-            )
+engine_nodes_BCM = Factory(name='BCM', 
+                description='Apply box counting method on scene', 
+                category='Fractal Analysis', 
+                nodemodule='engine_nodes',
+                nodeclass='BCM',
+                inputs=({'interface': None, 'name': 'Scene'}, {'interface': IFloat(min=3, max=16777216, step=1.000000), 'name': 'Stop Factor', 'value': 10}),
+                outputs=({'interface': ISequence, 'name': 'Scales'}, {'interface': ISequence, 'name': 'Intercepted Voxels'}),
+                widgetmodule=None,
+                widgetclass=None,
+                )
 
-###### end nodes definitions ###############
+
+
+
+engine_nodes_lactrix_fromPix = Factory(name='Pix2MatrixLac', 
+                description='Generate a MatrixLac from an Image', 
+                category='Fractal Analysis', 
+                nodemodule='engine_nodes',
+                nodeclass='lactrix_fromPix',
+                inputs=({'interface': IFileStr, 'name': 'Image path'}, {'interface': IFloat, 'name': 'Pixel width'}, {'interface': IDirStr, 'name': 'Save Directory', 'value': '/tmp'}),
+                outputs=({'interface': None, 'name': 'MatrixLac'}, {'interface': None, 'name': 'Thresholded image'}),
+                widgetmodule=None,
+                widgetclass=None,
+                )
+
+
+
+
+engine_nodes_lacunarity = Factory(name='Lacunarity', 
+                description='Compute lacunarity of n-dimensional matrix', 
+                category='Fractal Analysis', 
+                nodemodule='engine_nodes',
+                nodeclass='lacunarity',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+                )
+
+
+
+
+engine_nodes_lactrix_fromScene = Factory(name='Scene2MatrixLac', 
+                description='Generate a MatrixLac from PlantGL scene', 
+                category='Fractal Analysis', 
+                nodemodule='engine_nodes',
+                nodeclass='lactrix_fromScene',
+                inputs=({'interface': None, 'name': 'Scene'}, {'interface': IStr, 'name': 'Name'}, {'interface': IFloat(min=2, max=16777216, step=1.000000), 'name': 'Grid Size'}, {'interface': IDirStr, 'name': 'Save Directory', 'value': '/tmp'}, {'interface': IBool, 'name': 'Density', 'value': False}),
+                outputs=({'interface': None, 'name': 'MatrixLac'}, {'interface': None, 'name': 'Pgl scene'}),
+                widgetmodule=None,
+                widgetclass=None,
+                )
+
+
+
+
+engine_nodes_voxelize = Factory(name='Voxelize', 
+                description='Generates an embedding grid for a scene', 
+                category='Fractal Analysis', 
+                nodemodule='engine_nodes',
+                nodeclass='voxelize',
+                inputs=({'interface': None, 'name': 'Scene'}, {'interface': IInt, 'name': 'Division Factor', 'value': 10}, {'interface': IBool, 'name': 'Density', 'value': True}),
+                outputs=({'interface': IInt, 'name': 'Voxels size'}, {'interface': ISequence, 'name': 'Centers'}, {'interface': ISequence, 'name': 'Densities'}, {'interface': None, 'name': 'VoxScene'}),
+                widgetmodule=None,
+                widgetclass=None,
+                )
+
+
+
 
--- src/fractalysis/light/__wralea__.py
+++ src/fractalysis/light/__wralea__.py
@@ -1,119 +1,93 @@
-# -*- python -*-
-#
-#       OpenAlea.Fractalysis : OpenAlea fractal analysis library module
-#
-#       Copyright or (C) or Copr. 2006 INRIA - CIRAD - INRA  
-#
-#       File author(s): Da SILVA David <david.da_silva@cirad.fr>
-#
-#       Distributed under the Cecill-C License.
-#       See accompanying file LICENSE.txt or copy at
-#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
-# 
-#       OpenAlea WebSite : http://openalea.gforge.inria.fr
-#
+
+# This file has been generated at Thu Jul 31 17:10:57 2008
 
 from openalea.core import *
 
-__name__ = "fractalysis.light"
-__version__ = '0.0.1'
+
+__name__ = 'vplants.fractalysis.light'
+
+__editable__ = True
+__description__ = 'Fractalysis light nodes.'
 __license__ = 'CeCILL-C'
+__url__ = 'http://openalea.gforge.inria.fr'
+__alias__ = ['fractalysis.light']
+__version__ = '0.0.1'
 __authors__ = 'OpenAlea consortium'
 __institutes__ = 'INRIA/CIRAD'
-__description__ = 'Fractalysis light nodes.'
-__url__ = 'http://openalea.gforge.inria.fr'
-__icon__= 'light_icon.png' 
-__editable__ = 'False' 
+__icon__ = 'light_icon.png'
  
-__all__ = ['MSS', 'genPix', 'lightDir', 'lightInter', 'lightReciev']
 
-###### begin nodes definitions #############
-
-MSS = Factory( name="create MSS",
-              description="Generates a multi scale structure",
-              category="Light",
-              nodemodule="light_nodes",
-              nodeclass="create_MSS",
-              inputs=(dict(name="Name", interface=IStr, value='myMMS'),
-                      dict(name="Scene", interface=None,),
-                      dict(name="Scale table", interface=ISequence, value=None),
-                      dict(name="Enveloppe type", interface=IEnumStr(("Cvx Hull", "Sphere", "Ellipse", "Box")), value="Cvx Hull"),
-                      ),
-              outputs=(dict(name="MSS", interface = None,),
-                      ),
-              )
-
-genPix = Factory( name="generatePix",
-              description="Generates directional shadow picture",
-              category="Light",
-              nodemodule="light_nodes",
-              nodeclass="generate_pix",
-              inputs=(dict(name="MSS", interface=None,),
-                      dict(name="Light direction", interface=ISequence,),
-                      dict(name="Distribution", interface=ISequence,),
-                      dict(name="Image size", interface=IEnumStr(("100x100", "150x150", "200x200", "300x300", "600x600")), value="300x300"),
-                      dict(name="Save path", interface=IDirStr,),
-                      ),
-              outputs=(dict(name="Image", interface = None,),
-                      ),
-              )
-
-lightInter = Factory( name="Light interception",
-              description="Compute directional light interception as STAR values and shadow picture",
-              category="Light",
-              nodemodule="light_nodes",
-              nodeclass="light_intercept",
-              inputs=(dict(name="MSS", interface=None,),
-                      dict(name="Light direction", interface=ISequence, showwidget=False,),
-                      dict(name="Distribution", interface=ISequence),
-                      dict(name="Image size", interface=IEnumStr(("100x100", "150x150", "200x200", "300x300", "600x600")), value="150x150"),
-                      dict(name="Save path", interface=IDirStr,),
-                      ),
-              outputs=(dict(name="Star turbid", interface = IFloat,),
-                       dict(name="Star", interface = IFloat,),
-                       dict(name="Image", interface = None,),
-                      ),
-              )
+__all__ = ['light_nodes_light_direction', 'light_nodes_light_intercept', 'light_nodes_create_MSS', 'light_nodes_generate_pix', 'light_nodes_light_recieved']
 
 
-lightDir = Factory( name="Light direction",
-              description="Defines the direction of incident light",
-              category="Light",
-              nodemodule="light_nodes",
-              nodeclass="light_direction",
-              inputs= ( dict( name = "Direct directions", interface=IBool, value=True),
-                        dict( name = "Latitude", interface=IFloat, value=43.3643),
-                        dict( name = "Longitude", interface=IFloat, value=3.5238),
-                        dict( name = "Day", interface=IInt(1, 365), value=172),
-                        dict( name = "Start hour", interface=IFloat(0, 23.59, 0.5), value=7),
-                        dict( name = "Stop hour", interface=IFloat(0, 23.59, 0.5), value=19),
-                        dict( name = "Time step(min)", interface=IInt(1, 60), value=30),
-                        dict( name = "Turtle directions", interface=IBool, value=True),
-                        dict( name = "Sun shift", interface=IInt(1, 23), value=1, showwidget=False, hide=True),
-                        dict( name = "GMT shift", interface=IInt(1, 23), value=0, showwidget=False, hide=True),
-                      ),
-              outputs=( dict( name = "Sunlight directions", interface = ISequence),
-                      ),
-              )
-lightReciev = Factory( name="Recieved light",
-              description="Compute directional recieved light for one scale as relative ratio",
-              category="Light",
-              nodemodule="light_nodes",
-              nodeclass="light_recieved",
-              inputs=(dict(name="MSS", interface=None,),
-                      dict(name="Scale", interface=IInt(1), value=1),
-                      dict(name="Light direction", interface=ISequence, showwidget=False,),
-                      dict(name="Mode", interface=IEnumStr(("Multiscale", "Real", "Turbid")), value="Multiscale"),
-                      dict(name="Image size", interface=IEnumStr(("100x100", "150x150", "200x200", "300x300", "600x600")), value="150x150"),
-                      dict(name="Save path", interface=IDirStr,),
-                      ),
-              outputs=(dict(name="Recieved ratio", interface = IDict,),
-                       dict(name="PGL scene", interface = None,),
-                      ),
-              )
+
+light_nodes_light_direction = Factory(name='Light direction', 
+                description='Defines the direction of incident light', 
+                category='Light', 
+                nodemodule='light_nodes',
+                nodeclass='light_direction',
+                inputs=({'interface': IBool, 'name': 'Direct directions', 'value': True}, {'interface': IFloat, 'name': 'Latitude', 'value': 43.3643}, {'interface': IFloat, 'name': 'Longitude', 'value': 3.5238}, {'interface': IFloat(min=1, max=365, step=1), 'name': 'Day', 'value': 172}, {'interface': IFloat(min=0, max=23, step=0.500000), 'name': 'Start hour', 'value': 7}, {'interface': IFloat(min=0, max=23, step=0.500000), 'name': 'Stop hour', 'value': 19}, {'interface': IFloat(min=1, max=60, step=1), 'name': 'Time step(min)', 'value': 30}, {'interface': IBool, 'name': 'Turtle directions', 'value': True}, {'interface': IFloat(min=1, max=23, step=1), 'showwidget': False, 'hide': True, 'name': 'Sun shift', 'value': 1}, {'interface': IFloat(min=1, max=23, step=1), 'showwidget': False, 'hide': True, 'name': 'GMT shift', 'value': 0}),
+                outputs=({'interface': ISequence, 'name': 'Sunlight directions'},),
+                widgetmodule=None,
+                widgetclass=None,
+                )
 
 
-###### end nodes definitions ###############
+
+
+light_nodes_light_intercept = Factory(name='Light interception', 
+                description='Compute directional light interception as STAR values and shadow picture', 
+                category='Light', 
+                nodemodule='light_nodes',
+                nodeclass='light_intercept',
+                inputs=({'interface': None, 'name': 'MSS'}, {'interface': ISequence, 'showwidget': False, 'name': 'Light direction'}, {'interface': ISequence, 'name': 'Distribution'}, {'interface': IEnumStr(enum=('100x100', '150x150', '200x200', '300x300', '600x600')), 'name': 'Image size', 'value': '150x150'}, {'interface': IDirStr, 'name': 'Save path'}),
+                outputs=({'interface': IFloat, 'name': 'Star turbid'}, {'interface': IFloat, 'name': 'Star'}, {'interface': None, 'name': 'Image'}),
+                widgetmodule=None,
+                widgetclass=None,
+                )
+
+
+
+
+light_nodes_create_MSS = Factory(name='create MSS', 
+                description='Generates a multi scale structure', 
+                category='Light', 
+                nodemodule='light_nodes',
+                nodeclass='create_MSS',
+                inputs=({'interface': IStr, 'name': 'Name', 'value': 'myMMS'}, {'interface': None, 'name': 'Scene'}, {'interface': ISequence, 'name': 'Scale table', 'value': None}, {'interface': IEnumStr(enum=('Cvx Hull', 'Sphere', 'Ellipse', 'Box')), 'name': 'Enveloppe type', 'value': 'Cvx Hull'}),
+                outputs=({'interface': None, 'name': 'MSS'},),
+                widgetmodule=None,
+                widgetclass=None,
+                )
+
+
+
+
+light_nodes_generate_pix = Factory(name='generatePix', 
+                description='Generates directional shadow picture', 
+                category='Light', 
+                nodemodule='light_nodes',
+                nodeclass='generate_pix',
+                inputs=({'interface': None, 'name': 'MSS'}, {'interface': ISequence, 'name': 'Light direction'}, {'interface': ISequence, 'name': 'Distribution'}, {'interface': IEnumStr(enum=('100x100', '150x150', '200x200', '300x300', '600x600')), 'name': 'Image size', 'value': '300x300'}, {'interface': IDirStr, 'name': 'Save path'}),
+                outputs=({'interface': None, 'name': 'Image'},),
+                widgetmodule=None,
+                widgetclass=None,
+                )
+
+
+
+
+light_nodes_light_recieved = Factory(name='Recieved light', 
+                description='Compute directional recieved light for one scale as relative ratio', 
+                category='Light', 
+                nodemodule='light_nodes',
+                nodeclass='light_recieved',
+                inputs=({'interface': None, 'name': 'MSS'}, {'interface': IFloat(min=1, max=16777216, step=1), 'name': 'Scale', 'value': 1}, {'interface': ISequence, 'showwidget': False, 'name': 'Light direction'}, {'interface': IEnumStr(enum=('Multiscale', 'Real', 'Turbid')), 'name': 'Mode', 'value': 'Multiscale'}, {'interface': IEnumStr(enum=('100x100', '150x150', '200x200', '300x300', '600x600')), 'name': 'Image size', 'value': '150x150'}, {'interface': IDirStr, 'name': 'Save path'}),
+                outputs=({'interface': IDict, 'name': 'Recieved ratio'}, {'interface': None, 'name': 'PGL scene'}),
+                widgetmodule=None,
+                widgetclass=None,
+                )
+
 
 
 
